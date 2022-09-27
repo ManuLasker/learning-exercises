9c2864891d8de6ace3b06912f243a2b29f9e5077
ManuLasker <emanuelule2828@gmail.com>
2022-09-26 22:20:21 -0500

    add better solution, cleaner to sublist problem in rust: pd: rust is god

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,43 +6,25 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn is_super_list<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> bool {
-    if _second_list.is_empty() {
-        return true;
-    } else {
-        for i in 0.._first_list.len() {
-            if (_first_list[i] == _second_list[0]) && (i + _second_list.len() <= _first_list.len()) {
-                if (&_first_list[i..i+_second_list.len()]).eq(_second_list) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
-pub fn is_sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> bool {
-    return is_super_list(_second_list, _first_list);
-}
-
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let result:Comparison;
-    if _first_list.eq(_second_list) {
-        result = Comparison::Equal;
-    } else {
-        if _first_list.len() == _second_list.len() {
-            result = Comparison::Unequal;
-        } else if _first_list.len() > _second_list.len() {
-            match is_super_list(_first_list, _second_list) {
-                true => result = Comparison::Superlist,
-                false => result = Comparison::Unequal
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Comparison::Equal,     // both empty so they are equals
+        (0, _) => Comparison::Sublist,   // first empty, second whatever so first is sublist
+        (_, 0) => Comparison::Superlist, // first whatever, second empty so first is superlist
+        (m, n) if m > n => {
+            if _first_list.windows(n).any(|v| v == _second_list) {
+                Comparison::Superlist
+            } else {
+                Comparison::Unequal
             }
-        } else {
-            match is_sublist(_first_list, _second_list) {
-                true => result = Comparison::Sublist,
-                false => result = Comparison::Unequal
+        }, 
+        (m, n) if m < n => {
+            if _second_list.windows(m).any(|v| v == _first_list) {
+                Comparison::Sublist
+            } else {
+                Comparison::Unequal
             }
-        }
+        },
+        (_, _) => if _first_list.eq(_second_list) {Comparison::Equal} else {Comparison::Unequal}
     }
-    return result;
 }
