ebe59f49206457aabad73d71009727f5ffc5712a
ManuLasker <emanuelule2828@gmail.com>
2023-08-02 15:22:00 -0500

    day 4 finish

--- advent_of_code/2022/c/day_4/main.c
+++ advent_of_code/2022/c/day_4/main.c
@@ -6,13 +6,14 @@
 const char* filename = "data.txt";
 //const char *filename = "test_data.txt";
 
-
-int isContained(char* line){
+int *getNumbers(char* line){
     char* lineCpy = malloc(sizeof(line));
     strcpy(lineCpy, line);
+
     char* token = strtok_r(lineCpy, ",", &lineCpy);
     char** tokens = malloc(4 * sizeof(char*));
     int nNumbers = 0;
+
     for(int i=0;token;i++){
         char* tokenCopy = malloc(sizeof(token));
         strcpy(tokenCopy, token);
@@ -30,8 +31,26 @@ int isContained(char* line){
     for (int i=0;i<4;i++){
         numbers[i] = atoi(tokens[i]);
     }
+    return numbers;
+}
 
+int isContained(char* line){
+    int* numbers = getNumbers(line);
     if ((numbers[0] >= numbers[2] && numbers[1] <= numbers[3]) || (numbers[2] >= numbers[0] && numbers[3] <= numbers[1])){
+        free(numbers);
+        return 1;
+    } else {
+        free(numbers);
+        return 0;
+    }
+}
+
+int isPartiallyContained(char* line){
+    int* numbers = getNumbers(line);
+    if ( (numbers[0] >= numbers[2] && numbers[0] <= numbers[3]) ||
+         (numbers[1] >= numbers[2] && numbers[1] <= numbers[3]) ||
+         (numbers[2] >= numbers[0] && numbers[2] <= numbers[1]) ||
+         (numbers[3] >= numbers[0] && numbers[3] <= numbers[1])){
         free(numbers);
         return 1;
     } else {
@@ -54,8 +73,24 @@ int part1() {
     return sum;
 }
 
+int part2() {
+    int sum = 0;
+    FILE* fptr = NULL;
+    char line[1024];
+    fptr = fopen(filename, "r");
+    while(fgets(line, sizeof(line), fptr)){
+        //printf("line %s", line);
+        sum += isPartiallyContained(line);
+    }
+    // printf("still good\n");
+    fclose(fptr);
+    return sum;
+}
+
 int main() {
-    int total = part1();
-    printf("total repeated: %i", total);
+    int totalpart1 = part1();
+    int totalpart2 = part2();
+    printf("total part1 repeated: %i\n", totalpart1);
+    printf("total part2 repeated: %i\n", totalpart2);
     return 0;
 }
