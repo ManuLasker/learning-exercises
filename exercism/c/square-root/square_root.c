9ebc35b65414e7aa7e855a177d65af05605c235d
ManuLasker <emanuelule2828@gmail.com>
2023-08-08 20:29:55 -0500

    feat: finish two exercis from exercism

---
+++ exercism/c/square-root/square_root.c
@@ -0,0 +1,5 @@
+#include "square_root.h"
+
+uint16_t square_root(uint64_t x){
+    return sqrt(x);
+}
---
+++ exercism/c/square-root/square_root.h
@@ -0,0 +1,6 @@
+#ifndef SQUARE_ROOT_H
+#define SQUARE_ROOT_H
+#include <math.h>
+#include <stdint.h>
+uint16_t square_root(uint64_t x);
+#endif
