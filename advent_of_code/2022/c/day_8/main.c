21ed0d1daee1e9472cb0444f2ac9443bf623b8c4
ManuLasker <emanuelule2828@gmail.com>
2023-08-23 22:49:06 -0500

    feat: add day_8 for advent_of_code

--- advent_of_code/2022/c/day_8/main.c
+++ advent_of_code/2022/c/day_8/main.c
@@ -1,4 +1,6 @@
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // static const char* FILE_NAME = "test_data.txt";
 static const char* FILE_NAME = "data.txt";
@@ -29,34 +31,13 @@ cleanline(char *line){
     }
 }
 
-dim
-get_dimension(){
-    dim dimension;
-    dimension.n = 0;
-    dimension.m = 0;
-
-    FILE *fptr = NULL;
-    char line[1024];
-
-    fptr = fopen(FILE_NAME, "r");
-
-    while(fgets(line, sizeof(line), fptr)){
-        cleanline(line);
-        dimension.m = len(line);
-        (dimension.n)++;
-    }
-
-    fclose(fptr);
-    return dimension;
-}
-
 int
 char_to_int(char c){
     return c - '0';
 }
 
 void
-debug_mapping(dim dimension, int m[][dimension.m]){
+debug_mapping(dim dimension, int **m){
     int i, j;
     for(i=0; i<dimension.n; i++){
         for(j=0; j<dimension.m; j++){
@@ -67,7 +48,7 @@ debug_mapping(dim dimension, int m[][dimension.m]){
 }
 
 int
-validate_all_directions(dim dimension, int mapping[][dimension.m],
+validate_all_directions(dim dimension, int **mapping,
         int i, int j){
     int sw_l = 1, sw_r = 1, sw_u = 1, sw_d = 1;
 
@@ -112,7 +93,7 @@ validate_all_directions(dim dimension, int mapping[][dimension.m],
 }
 
 int
-get_total_visible_trees(dim dimension, int mapping[][dimension.m]){
+get_total_visible_trees(dim dimension, int **mapping){
     int initial_visible_trees = ((dimension.m - 2) * 2) + dimension.n * 2;
     int count = 0;
     int sw = 0;
@@ -129,30 +110,92 @@ get_total_visible_trees(dim dimension, int mapping[][dimension.m]){
 }
 
 int
+get_scenic_score(dim dimension, int **mapping, int i, int j){
+    int sw_l = 0, sw_r = 0, sw_u = 0, sw_d = 0;
+
+    // all left
+    for (int k = j - 1; k >= 0; k--){
+        sw_l++;
+        if (mapping[i][k] >= mapping[i][j]){
+            break;
+        }
+    }
+    // all right
+    for (int k = j + 1; k < dimension.m; k++){
+        sw_r++;
+        if (mapping[i][k] >= mapping[i][j]){
+            break;
+        }
+    }
+    // all up
+    for (int k = i - 1; k >= 0; k--){
+        sw_u++;
+        if (mapping[k][j] >= mapping[i][j]){
+            break;
+        }
+    }
+    // all down
+    for (int k = i + 1; k < dimension.n; k++){
+        sw_d++;
+        if (mapping[k][j] >= mapping[i][j]){
+            break;
+        }
+    }
+    // printf("sw_l: %i, sw_r: %i, sw_u: %i, sw_d: %i: mapping %i\n", sw_l, sw_r, sw_u, sw_d, mapping[i][j]);
+    return sw_d * sw_u * sw_l * sw_r;
+}
+
+int
+get_max_scenic_score(dim dimension, int **mapping){
+    int max = INT_MIN;
+    int scenic_score = 0;
+    for(int i=0; i<dimension.n; i++){
+        for(int j=0; j<dimension.m; j++){
+            scenic_score = get_scenic_score(dimension, mapping, i, j);
+            if(max <= scenic_score){
+                max = scenic_score;
+            }
+        }
+    }
+    return max;
+}
+
+int
 main(){
     FILE *fptr = NULL;
-    dim dimension = get_dimension();
+    dim dimension;
+
+    dimension.n = 0;
+    dimension.m = 0;
+
     int i=0, j=0;
 
     fptr = fopen(FILE_NAME, "r");
 
-    int mapping[dimension.n][dimension.m];
+    int **mapping = (int**) malloc(sizeof(int*) * (i + 1));
     int c;
 
     while((c = fgetc(fptr)) != EOF){
         if (c == '\n'){
             i++;
+            mapping = (int**) realloc(mapping, sizeof(int*) * (i + 1));
+            dimension.m = j;
             j = 0;
         }else{
+            mapping[i] = (int*) realloc(mapping[i], sizeof(int) * (j + 1));
             mapping[i][j] = char_to_int(c);
             j++;
         }
     }
+    dimension.n = i;
 
     // debug_mapping(dimension, mapping);
     fclose(fptr);
 
     int total_visible_trees = get_total_visible_trees(dimension, mapping);
+    int max_scenic_score = get_max_scenic_score(dimension, mapping);
 
+    free(mapping);
     printf("total_visible_trees: %i\n", total_visible_trees);
+    printf("max_scenic_score: %i\n", max_scenic_score);
 }
